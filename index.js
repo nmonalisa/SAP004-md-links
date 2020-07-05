add8e2e74a17b03b63dea1030523f9b96b84e169
N. Monalisa <nathaliamonalisa@gmail.com>
2020-07-05 18:37:24 -0300

    Inciando intergração da função validateLink no arquivo principal

--- index.js
+++ index.js
@@ -10,13 +10,23 @@ const mdLinks = function (path, options) {
       .then(text => {
         let linkList = [];
         text.match(regex).map(item => {
-          const splitedContent = item.split('](');
-          linkList.push({
-            href: splitedContent[1].slice(0, -1),
-            text: splitedContent[0].slice(1).substr(0, 50),
-            path
+            const splitedContent = item.split('](');
+            linkList.push({
+              href: splitedContent[1].slice(0, -1),
+              text: splitedContent[0].slice(1).substr(0, 50),
+              path
+            })
           })
-        })
+          // linkList.map(item => {
+          //   axios.get(link.href)
+          //   .then(result => {
+          //     console.log(result.status)
+          //     result.status == 200 ? link.status = 'OK' : link.status = 'fail'
+          //   })
+          //   .catch(error => {
+          //     console.log('Não foi possível validar o link');
+          //   })
+          // })
         resolve(linkList);
       })
       .catch(error => console.log('Ocorreu um erro: arquivo ou diretório não encontrado'))
--- validator.js
+++ validator.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
 
-
 let meuLink = {
+  href: 'https://www.udemy.com/',
   status: ''
 };
 
-const validateLink = function (url, link) {
-  axios.get(url).
+const validateLink = function (link) {
+  axios.get(link.href).
   then(result => {
       console.log(result.status)
       result.status == 200 ? link.status = 'OK' : link.status = 'fail'
@@ -17,15 +17,4 @@ const validateLink = function (url, link) {
     })
 
 };
-validateLink('https://www.udemy.com/', meuLink)
-
-
-
-//Preciso extrair o servidor
-
-// const validateLink = function (url) {
-//     axios.get(url).then(response => {
-//       response.status == 200 ? console.log('OK') : console.log('fail')
-//     }).catch(() => console.log('O link não pode ser validado.'));
-//   }
-//   validateLink('https://wwww.google.com.br')
+validateLink(meuLink)
