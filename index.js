fd8f3f6c7cbb10b4e710a60ae19c5c78da4f301d
N. Monalisa <nathaliamonalisa@gmail.com>
2020-07-09 11:43:44 -0300

    Refatorei a função mdLinks

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
 // Contém a função principal e genérica (mdLinks) que manipula arquivos.md em busca de links
 
-const fileContent = require('./reader');
-const validateLink = require('./validator')
+// const fileContent = require('./reader');
+const axios = require('axios')
+const fs = require('fs').promises;
 const regex = /(\[[^\s].*?\])(\(https?:+[^\s]+[\w/])\)/gm;
+
+
+
+const getFile = (path) => fs.readFile(path, 'utf-8', data => {
+  resolve(data);
+  reject('Não foi possível ler o seu arquivo.')
+});
+
+const extractMdLinks = (text, regex, cutBy) => {
+  const linkList = []
+  text.match(regex).forEach(item => {
+    const splitedContent = item.split(cutBy);
+    linkList.push({
+      href: splitedContent[1].slice(0, -1),
+      text: splitedContent[0].slice(1).substr(0, 50),
+    })
+  });
+  return linkList;
+}
+
+
+const mergeStatus = (links, status) => {
+  return links.map((link, index) => ({
+    ...link,
+    status: status[index]
+  }))
+}
+
+const validateLink = function (linklist) {
+  return new Promise(resolve => {
+    const validate = linklist.map(link => {
+      return axios.get(link.href)
+        .then(result => {
+          return result.status === 200 ? 'OK' : 'fail'
+        })
+        .catch(() => {
+          return 'fail'
+        });
+    })
+    Promise.all(validate)
+      .then(result => {
+        resolve(mergeStatus(linklist, result));
+      })
+  })
+};
+
 
 const mdLinks = function (path, options) {
   return new Promise((resolve, reject) => {
-    fileContent(path)
+    getFile(path)
       .then(text => {
-        let linkList = [];
-        text.match(regex).map(item => {
-          const splitedContent = item.split('](');
-          linkList.push({
-            href: splitedContent[1].slice(0, -1),
-            text: splitedContent[0].slice(1).substr(0, 50),
-            path,
-            status: '?'
-          })
-        })
+        const linkList = extractMdLinks(text, regex, '](')
+
+        // Validação dos links
         if (options === '--validate') {
           validateLink(linkList)
             .then(result => resolve(result))
@@ -26,7 +66,7 @@ const mdLinks = function (path, options) {
           resolve(linkList);
         }
       })
-      .catch(() => console.log('Ocorreu um erro: arquivo ou diretório não encontrado'))
+      .catch((error) => console.log(error))
   });
 };
 
--- reader.js
+++ reader.js
@@ -1,3 +1,3 @@
-const fs = require('fs').promises;
-const fileContent = path => fs.readFile(path, 'utf-8')
-module.exports = fileContent
+// const fs = require('fs').promises;
+// const fileContent = path => fs.readFile(path, 'utf-8')
+// module.exports = fileContent
